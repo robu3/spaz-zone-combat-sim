159db5dc7b9768d7678f4450c1d018c0f2617e82
Rob <robu@shortmail.com>
2015-02-13 12:06:35 -0500

    adding function descriptions

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var random = require("./lib/random");
 
+var results = [];
 for (var i = 0; i < 100; i++) {
-	console.log(random.dice("2d6+1"));
+	results.push(random.roll("2d6+1"));
 }
+
+results.sort(function (a, b) {
+	if (a < b) {
+		return -1;
+	}
+
+	if (a > b) {
+		return 1;
+	}
+
+	return 0;
+});
+
+console.log(results);
