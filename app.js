cf353d99a7321933e628d3c788eb3dde1ec1f6e5
Rob <robu@shortmail.com>
2015-05-27 21:46:22 -0400

    standing up the web app

--- app.js
+++ app.js
@@ -1,20 +1,70 @@
 var express = require("express"),
 	config = require("./config/config.js"),
 	Weapons = require("./lib/weapons"),
-	app = express();
+	app = express(),
+	bodyParser = require("body-parser"),
+
+	Character = require("./lib/character"),
+	Battle = require("./lib/battle"),
+	config = require("./config/config.js");
 
 app.set("view engine", "jade");
 app.use("/public", express.static("public"));
+app.use(bodyParser.json());
 app.use("/public/jquery", express.static("node_modules/jquery"));
 app.use("/public/bootstrap", express.static("node_modules/bootstrap"));
 app.use("/public/angular", express.static("node_modules/angular"));
+app.use("/public/angular-resource", express.static("node_modules/angular-resource"));
 
+app.use("/public/results", express.static("results"));
+
+// GET /
+// return the main page
 app.get("/", function (request, response) {
 	//response.send("Hello, World!");
 	response.render("index", { title: "Spaz Zone Combat Sim" });
 });
 
-app.get("/weapons", function (request, response) {
+// POST /
+// run the simulation, generating some results
+app.post("/api/simulation", function (request, response) {
+	// build combatants from request
+	var combatant1 = new Character(
+		request.body.combatant1.name,
+		request.body.combatant1.stats
+	);
+	combatant1.recalculateDerivedStats();
+	combatant1.equip(request.body.combatant1.weapon);
+
+	console.log(combatant1);
+
+	var combatant2 = new Character(
+		request.body.combatant2.name,
+		request.body.combatant2.stats
+	);
+	combatant2.recalculateDerivedStats();
+	combatant2.equip(request.body.combatant2.weapon);
+
+
+	// execute battles
+	var battleCount = request.body.battleCount - 1,
+		battleResults = [{ battle: "Battle", victor: "Victor" }],
+		conf = {
+			timestamp: Date.now().toString(),
+			resultsFolder: "./results/"
+		};
+
+	Battle.executeMultiple(combatant1, combatant2, battleCount, battleResults, conf, function (roundsPath, battlesPath) {
+
+		// send file URLs back to client
+		response.send({
+			roundsUrl: "/public" + roundsPath.substring(1, roundsPath.length),
+			battlesUrl: "/public" + battlesPath.substring(1, battlesPath.length)
+		});
+	}, true);
+});
+
+app.get("/api/weapons", function (request, response) {
 	//response.send("Hello, World!");
 	var weapons = new Weapons();
 	weapons.fetch(function (err, data) {
--- public/js/apps/combatSimApp.js
+++ public/js/apps/combatSimApp.js
@@ -1,26 +1,79 @@
-var combatSimApp = angular.module("combatSimApp", []);
+var combatSimApp = angular.module("combatSimApp", ["ngResource"]);
 
-combatSimApp.controller("CombatSimController", function ($scope) {
+// simulation model
+//
+// properties should include:
+//
+// - combatant1: combatant 1
+// - combatant2: combatant 2
+// - battleCount: # of battles
+combatSimApp.factory("Simulation", function ($resource) {
+	return $resource(
+		"/api/simulation/:id",
+		null,
+		{
+			"update": "PUT"
+		}
+	);
+});
+
+// weapons list
+combatSimApp.factory("Weapon", function ($resource) {
+	return $resource("/api/weapons");
+});
+
+
+combatSimApp.controller("CombatSimController", function ($scope, Simulation, Weapon) {
 	console.log("angular loaded and running");
 
 	function buildDefaultCombatant() {
 		return {
 			name: "Sludgehead",
-			power: 10,
-			brawn: 10,
-			agility: 10,
-			brains: 10,
-			cunning: 10,
-			control: 10,
-			looks: 10
+			stats: {
+				power: 10,
+				brawn: 10,
+				agility: 10,
+				brains: 10,
+				cunning: 10,
+				control: 10,
+				looks: 10
+			}
 		}
 	}
 
+	// build default combatants
 	$scope.combatant1 = buildDefaultCombatant();
 	$scope.combatant1.name += " #1";
 
 	$scope.combatant2 = buildDefaultCombatant();
 	$scope.combatant2.name += " #2";
+
+	// populate weapons lists
+	Weapon.query(function (weapons) {
+		console.log("HERE");
+		$scope.weapons = weapons;
+	});
+
+
+	// submit click
+	$scope.onSubmitClick = function () {
+		// clear existing
+		$scope.downloadUrlRounds = null;
+		$scope.downloadUrlBattles = null;
+
+		var sim = new Simulation();
+
+		sim.combatant1 = $scope.combatant1;
+		sim.combatant2 = $scope.combatant2;
+		sim.battleCount = 100;
+
+		sim.$save(function (data) {
+			console.log("sim complete", data);
+
+			$scope.downloadUrlRounds = data.roundsUrl;
+			$scope.downloadUrlBattles = data.battlesUrl;
+		});
+	};
 });
 
 
